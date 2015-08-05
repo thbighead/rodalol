2f11faf54c94153124293ae171fb7e1a76771839
thcmatias <thcmatias@gmail.com>
2015-08-04 21:31:15 -0300

    dando push de novo apra o repositorio

---
+++ resources/views/masterpage.blade.php
@@ -0,0 +1,61 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <title>RodaLOL inform&aacute;tica</title>
+</head>
+<body>
+<header>
+    <div>
+        <figure>
+            <img src="" alt="">
+        </figure>
+        <form action="" method="post">
+            <label for="login">Login:</label>
+            <input type="text" name="login" id="login">
+            <br>
+            <label for="senha">Senha:</label>
+            <input type="password" name="senha" id="senha">
+        </form>
+    </div>
+    <br>
+    <section name="Hbar">
+        <nav>
+            <ul>
+                <li>
+                    <a href="<?php echo url('/'); ?>">In&iacute;cio</a>
+                </li>
+                <li>
+                    <a href="<?php echo url('contato'); ?>">
+                        <img src="imgs/phone.png" width="2%" height="2%" title="Contato"> Contato
+                    </a>
+                </li>
+                <li>
+                    <a href="">Quem somos?</a>
+                </li>
+                <li>
+                    <a href="admin">&Aacute;rea administrativa</a>
+                </li>
+            </ul>
+        </nav>
+    </section>
+</header>
+<section name="Vbar">
+    <nav>
+        <ul>
+            <li><a href="">Processadores</a></li>
+            <li><a href="">Mem&oacute;rias RAM</a></li>
+            <li><a href="">Monitores</a></li>
+            <li><a href="">Placas de v&iacute;deo</a></li>
+            <li><a href="">Armazenamento</a></li>
+            <li><a href="">Perif&eacute;ricos</a></li>
+        </ul>
+    </nav>
+</section>
+<footer>
+    <div>
+        P&aacute;gina feita pela <a href="">EJCM</a>.
+    </div>
+</footer>
+</body>
+</html>
