22188ed47a7f13a61e2c66c224042549046b7d25
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-10 21:27:55 -0300

    footer sempre 'colado' na parte de baixo da viewport

--- resources/views/templates/footer.blade.php
+++ resources/views/templates/footer.blade.php
@@ -1,44 +1,46 @@
-<section id="information" class="color-light" >
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-4 col-md-4 col-lg-4">
-                <h4 class="color-green">INFORMAÇÕES</h4>
-                <ul class="decoration text-left">
-                    <li><a href="{{route('quemSomos')}}" class="color-light">Quem Somos</a></li>
-                    <li><a href="#" class="color-light">Termos e Condições</a></li>
-                    <li><a href="#" class="color-light">Políticas de Privacidade</a></li>
-                </ul>
-            </div>
+<footer>
+    <section id="information" class="color-light" >
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <h4 class="color-green">INFORMAÇÕES</h4>
+                    <ul class="decoration text-left">
+                        <li><a href="{{route('quemSomos')}}" class="color-light">Quem Somos</a></li>
+                        <li><a href="#" class="color-light">Termos e Condições</a></li>
+                        <li><a href="#" class="color-light">Políticas de Privacidade</a></li>
+                    </ul>
+                </div>
 
-            <div class="col-sm-4 col-md-4 col-lg-4">
-                <h4 class="color-green">MINHA CONTA</h4>
-                <ul class="decoration text-left">
-                    <li><a href="#" class="color-light">Meu Cadastro</a></li>
-                    <li><a href="#" class="color-light">Meus Pedidos</a></li>
-                    <li><a href="#" class="color-light">Meu Carrinho</a></li>
-                </ul>
-            </div>
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <h4 class="color-green">MINHA CONTA</h4>
+                    <ul class="decoration text-left">
+                        <li><a href="#" class="color-light">Meu Cadastro</a></li>
+                        <li><a href="#" class="color-light">Meus Pedidos</a></li>
+                        <li><a href="#" class="color-light">Meu Carrinho</a></li>
+                    </ul>
+                </div>
 
-            <div class="col-sm-4 col-md-4 col-lg-4">
-                <h4 class="color-green">CONTATO</h4>
-                <adress>
-                    <strong>RodaLOL</strong><br>
-                    Rua das Muambas, 171, Centro - Rio de Janeiro<br>
-                    CEP: 25171-000<br><br>
-                    <abbr title="Telefone">Tel: </abbr> +55 (21) 1711-7171
-                    <br><br>
-                    <strong>Email</strong><br>
-                    <a href="#" class="color-green">contato@rodalol.com</a>
-                </adress>
-            </div>
-        </div><!--    .row    -->
-    </div><!--  .container  -->
-</section><!--    #information    -->
+                <div class="col-sm-4 col-md-4 col-lg-4">
+                    <h4 class="color-green">CONTATO</h4>
+                    <adress>
+                        <strong>RodaLOL</strong><br>
+                        Rua das Muambas, 171, Centro - Rio de Janeiro<br>
+                        CEP: 25171-000<br><br>
+                        <abbr title="Telefone">Tel: </abbr> +55 (21) 1711-7171
+                        <br><br>
+                        <strong>Email</strong><br>
+                        <a href="#" class="color-green">contato@rodalol.com</a>
+                    </adress>
+                </div>
+            </div><!--    .row    -->
+        </div><!--  .container  -->
 
-<footer id="copyright">
-    <div class="container-fluid text-center color-light">
-        <p>
-            <a href="http://www.ejcm.com.br/" class="color-green">EJCM 2015</a> &copy; Todos os Direitos Reservados.
-        </p>
-    </div><!--  .container-fluid  -->
+        <section id="copyright">
+            <div class="container-fluid text-center color-light">
+                <p>
+                    <a href="http://www.ejcm.com.br/" class="color-green">EJCM 2015</a> &copy; Todos os Direitos Reservados.
+                </p>
+            </div><!--  .container-fluid  -->
+        </section>
+    </section><!--    #information    -->
 </footer>
