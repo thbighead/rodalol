a29a4dca4f0b9bc5f7ea4a5e1855e6e4faf9f74f
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-11 23:15:18 -0300

    * form de login melhorado e adicionado link (que nao ta funcionando) pra recueprar senha. Corrigir link pro modal de recuperação de senha.

--- resources/views/templates/header.blade.php
+++ resources/views/templates/header.blade.php
@@ -33,14 +33,16 @@
                     <button type="button" class="close" data-dismiss="modal">&times;</button>
                     <h4 class="modal-title">Entre com sua conta</h4>
                 </div>
-                <div class="modal-body">
-                    <form action="#" class="form-horizontal">
+                <div class="modal-body" id="login">
+                    <form method="post" action="#" class="form-horizontal" name="login_form">
+                        {{--Email--}}
                         <div class="form-group">
                             <label for="login" class="col-md-2 control-label">E-mail</label>
                             <div class="col-md-8">
                                 <input type="text" class="form-control" placeholder="exemplo&#64;exemplo.com" name="login" id="login">
                             </div>
                         </div>
+                        {{--Senha--}}
                         <div class="form-group">
                             <label for="password" class="col-md-2 control-label">Senha</label>
                             <div  class="col-md-8">
@@ -49,10 +51,23 @@
                         </div>
                     </form>
                 </div>
+                {{--Botões de entrar e esqueci a senha--}}
                 <div class="modal-footer">
-                    <button type="submit" class="btn btn-default" data-dismiss="modal">Entrar</button>
+                    <div class="row col-md-10 col-md-offset-1">
+                        <button type="submit" class="btn btn-lg btn-primary btn-block" data-dismiss="modal">Entrar</button>
+                    </div>
+                    <a href="#forgotpassword" data-toggle="tab" class="text-center col-md-12 col-xs-12"> Esqueci a senha?</a>
                 </div>
             </div>
+                <div class="modal fade" id="forgotpassword">
+                    <form method="post" action="" name="forgot_password">
+                        Esqueceu sua senha? Nos mande seu email e nós resetaremos!
+                        <input type="email" placeholder="exemplo&#64;exemplo.com" name="login" id="login">
+                        <button type="submit" class="btn btn-lg btn-primary btn-block" data-dismiss="modal">Enviar</button>
+                        <a href="#login" data-toggle="tab">Espere, consegui lembrar!</a>
+                    </form>
+                </div>
+
         </div>
     </div>
     <!--    menu principal  -->
