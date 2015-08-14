9017401ec0306622fd4ea988a0e9fc605ebcb4ee
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-13 21:55:00 -0300

    + header area admin

---
+++ resources/views/templates/admin/header.blade.php
@@ -0,0 +1,36 @@
+<header>
+    <nav class="navbar navbar-inverse">
+        <div class="container">
+            <!-- Brand and toggle get grouped for better mobile display -->
+            <div class="navbar-header">
+                <a href="#" class="navbar-brand">RODALOL
+                    {{--<img class="img-logo" src="{{asset('img/logo.png')}}" alt="Logo RodaLOL">--}}
+                </a>
+
+                <button class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-admin">
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+            </div>
+
+            <div class="collapse navbar-collapse" id="navbar-collapse-admin">
+                <ul class="nav navbar-nav navbar-right">
+                    <li class="dropdown">
+                        <a href="#" class="dropdown-toggle" role="button" data-toggle="dropdown">
+                            <i class="glyphicon glyphicon-user"></i> Admin <span class="caret"></span>
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a href="#">Meu Perfil</a></li>
+                            <li><a href="#"><i class="glyphicon glyphicon-lock"></i> Logout</a></li>
+                        </ul>
+                    </li>
+                </ul>
+            <!--    .navbar-collapse    -->
+            </div>
+        <!--    .container    -->
+        </div>
+    </nav>
+</header>
+
+
