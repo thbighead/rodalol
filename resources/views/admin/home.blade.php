dc849c737d90f62c700c26ea38583b0621243090
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-19 16:15:34 -0300

    # corrigido link de script js

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -8,91 +8,49 @@
 
 <div class="container">
     <div class="row">
-
-        <div class="col-md-3 col-sm-3">
-            <h3>Opções</h3>
-            <hr>
-            {{--MENU--}}
-            <nav class="navbar navbar-default sidebar" role="navigation">
-                {{--<div class="container">--}}
-                <!-- Brand and toggle get grouped for better mobile display -->
-                <div class="navbar-header">
-                    <a href="#" class="navbar-brand">Menu
-                    {{--<img class="img-logo" src="{{asset('img/logo.png')}}" alt="Logo RodaLOL">--}}
-                    </a>
-                    <button class="navbar-toggle" data-toggle="collapse" data-target="#navbar-collapse-sidebar">
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                        <span class="icon-bar"></span>
-                    </button>
-                </div><!-- .navbar-header -->
-
-                <div class="collapse navbar-collapse text-left" id="navbar-collapse-sidebar">
-                    <ul class="nav nav-pills nav-stacked">
-                        <li class="active"><a href="#">Dashboard</a></li>
-                        {{-- Comments Dropdown --}}
-                        <li class="dropdown"><a href="{{route('AddProduto')}}"><i class="glyphicon glyphicon-comment"></i>&nbsp;&nbsp;Comentários</a></li>
-                        {{-- Graphics Dropdown --}}
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-stats"></i>
-                                    &nbsp;&nbsp;Gráficos&nbsp;&nbsp;<span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Submenu 1-1</a></li>
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Submenu 1-2</a></li>
-                                <li class="divider"></li>
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Submenu 1-3</a></li>
-                            </ul>
-                        </li>
-                        {{-- Products Dropdown --}}
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-shopping-cart"></i>
-                                &nbsp;&nbsp;Produtos&nbsp;&nbsp;<span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="{{route('AddProduto')}}" class="dropdown-toggle" data-toggle="dropdown">Adicionar Produtos</a></li>
-                                <li><a href="{{route('ListProduto')}}" class="dropdown-toggle" data-toggle="dropdown">Alterar Produtos</a></li>
-                                <li><a href="{{route('ListProduto')}}" class="dropdown-toggle" data-toggle="dropdown">Excluir Produtos</a></li>
-                            </ul>
-                        </li>
-                        {{-- Slideshow Dropdown --}}
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-picture"></i>
-                            &nbsp;&nbsp;Slideshow&nbsp;&nbsp;<span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Cadastrar Imagens</a></li>
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Listar Imagens</a></li>
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Alterar Imagens</a></li>
-                            </ul>
-                        </li>
-                        {{-- Texts Dropdown --}}
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-pencil"></i>
-                                &nbsp;&nbsp;Textos&nbsp;&nbsp;<span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Página Inicial</a></li>
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Sobre a Empresa</a></li>
-                            </ul>
-                        </li>
-                        {{-- Users Dropdown --}}
-                        <li>
-                            <a href="#" class="dropdown-toggle" data-toggle="dropdown"><i class="glyphicon glyphicon-user"></i>
-                                &nbsp;&nbsp;Usuários&nbsp;&nbsp;<span class="caret"></span>
-                            </a>
-                            <ul class="dropdown-menu" role="menu">
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Listar Usuários</a></li>
-                                <li><a href="#" class="dropdown-toggle" data-toggle="dropdown">Excluir Usuários</a></li>
-                            </ul>
-                        </li>
-                    </ul>
-                </div><!-- .collapse navbar-collapse -->
-                {{--</div><!-- .container -->--}}
-            </nav>
-            <hr>
-            {{--FIM MENU--}}
+        <div class="col-sm-2">
+            <div class="sidebar-nav">
+                <div class="navbar navbar-default" role="navigation">
+                    <div class="navbar-header">
+                        <button type="button" class="navbar-toggle" data-toggle="collapse" data-target=".sidebar-navbar-collapse">
+                            <span class="sr-only">Toggle navigation</span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                            <span class="icon-bar"></span>
+                        </button>
+                        <span class="visible-xs navbar-brand">Sidebar menu</span>
+                    </div>
+                    <div class="navbar-collapse collapse sidebar-navbar-collapse">
+                        <ul class="nav navbar-nav">
+                            {{--<li class="active">--}}
+                                {{--<a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Dashboard<span class="caret"></span>--}}
+                            {{--</li>--}}
+                            <li>
+                                <a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Comentários<span class="caret"></span>
+                            </li>
+                            <li>
+                                <a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Gráficos<span class="caret"></span>
+                            </li>
+                            <li>
+                                <a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Produtos<span class="caret"></span>
+                            </li>
+                            <li>
+                                <a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Slideshow<span class="caret"></span>
+                            </li>
+                            <li>
+                                <a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Textos<span class="caret"></span>
+                            </li>
+                            <div class="clearfix"></div>
+                            <li>
+                                <a href="#"></a><i class="glyphicon glyphicon-picture"></i>&nbsp;&nbsp;Usuários<span class="caret"></span>
+                            </li>
+                        </ul>
+                    </div><!--/.nav-collapse -->
+                </div>
+            </div>
         </div>
+
+
 
         <div class="col-md-9 col-sm-9">
             <h3 class="pdHeading">Dashboard</h3>
