daa001b8b2e46414bcfc88ec48225b68d9d4906f
thcmatias <thcmatias@gmail.com>
2015-08-20 11:01:46 -0300

    +* criando e relacionando as migrations

---
+++ database/migrations/2015_08_20_135455_create_orders_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('quatidade')->unsigned();
+            $table->boolean('finalizado');
+
+            //foreign keys
+            $table->integer('idUser')->unsigned();
+            $table->foreign('idUser')->references('id')->on('users');
+            $table->integer('idProduct')->unsigned();
+            $table->foreign('idProduct')->references('id')->on('products');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('orders');
+    }
+}
