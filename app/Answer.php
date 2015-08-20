281014908fa9544d474c1117da610911d30e3ba0
thcmatias <thcmatias@gmail.com>
2015-08-20 10:11:43 -0300

    * models prontas e relacionadas de acordo com o MER (MER atualizado no drive)

--- app/Answer.php
+++ app/Answer.php
@@ -6,5 +6,29 @@ use Illuminate\Database\Eloquent\Model;
 
 class Answer extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'answers';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ["conteudo"];
+
+    //Get the user that owns the answer
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
+
+    //Get the comment that owns the order
+    public function comment()
+    {
+        return $this->belongsTo('App\Comment');
+    }
 }
--- app/Text.php
+++ app/Text.php
@@ -6,5 +6,17 @@ use Illuminate\Database\Eloquent\Model;
 
 class Text extends Model
 {
-    //
+    /**
+     * The database table used by the model.
+     *
+     * @var string
+     */
+    protected $table = 'texts';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = ["pagina", "conteudo"];
 }
