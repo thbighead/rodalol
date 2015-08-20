dd7c8d15a39855503fc89725fb35812176429b1b
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-20 19:50:03 -0300

    *+ seeder para tres categorias

--- database/seeds/ProductTableSeeder.php
+++ database/seeds/ProductTableSeeder.php
@@ -11,14 +11,25 @@ class ProductTableSeeder extends Seeder
      */
     public function run()
     {
-        for($i=0; $i < 100;$i++) {
+        for($i=0; $i < 20;$i++) {
             DB::table('products')->insert([
                 'nome' => str_random(30),
-                'categoria' => str_random(10),
-                'especificacao' => str_random(100),
+                'categoria' => 'Processador',
+                'especificacao' => 'Descrição aqui, especificação do produto. Vem que tem. Doge Doge Doge',
                 'preco' => mt_rand(1, 9999),
                 'qtdEstoque' => mt_rand(1, 1000),
             ]);
         }
+
+	    for($i=0; $i < 20;$i++) {
+		    DB::table('products')->insert([
+			    'nome' => str_random(30),
+			    'categoria' => 'Memória',
+			    'especificacao' => 'Descrição aqui, especificação do produto. Vem que tem. Doge Doge Doge',
+			    'preco' => mt_rand(1, 9999),
+			    'qtdEstoque' => mt_rand(1, 1000),
+		    ]);
+	    }
+
     }
 }
