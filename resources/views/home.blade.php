8793f862985fb91e14f4fab234db3291f992740c
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-20 19:57:51 -0300

    + section de texto estatico da home

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -148,11 +148,29 @@
             </div><!--  products 8   -->
         </div><!--  .row 2 -->
 
-        <div class="row text-center">
+        <div class="row text-center verticalMarginModal">
             <h3>
                 <a href="{{route('produtos')}}" class="color-green">Ver todos os Produtos</a>
             </h3>
         </div>
+
     </div><!--    .container    -->
+
+    <section>
+        <div class="container">
+            <p>Todos as descrições das pessoas são sobre a humanidade do atendimento, a pessoa pega no pulso, examina, olha com carinho.
+                Então eu acho que vai ter outra coisa, que os médicos cubanos trouxeram pro brasil, um alto grau de humanidade.
+            <p>Ai você fala o seguinte: "- Mas vocês acabaram isso?" Vou te falar: -"Não, está em andamento!" Tem obras que "vai" durar pra depois
+                de 2010. Agora, por isso, nós já não desenhamos, não começamos a fazer projeto do que nós "podêmo fazê"? 11, 12, 13, 14...
+                Por que é que não?</p>
+            <p>A população ela precisa da Zona Franca de Manaus, porque na Zona franca de Manaus, não é uma zona de exportação,
+                é uma zona para o Brasil. Portanto ela tem um objetivo, ela evita o desmatamento, que é altamente lucravito.
+                Derrubar arvores da natureza é muito lucrativo!</p>
+            <p>No meu xinélo da humildade eu gostaria muito de ver o Neymar e o Ganso. Por que eu acho que.... 11 entre 10 brasileiros gostariam.
+                Você veja, eu já vi, parei de ver. Voltei a ver, e acho que o Neymar e o Ganso têm essa capacidade de fazer a gente olhar.</p>
+            <p>Eu dou dinheiro pra minha filha. Eu dou dinheiro pra ela viajar, então é... é... Já vivi muito sem dinheiro, já vivi muito com dinheiro.
+                -Jornalista: Coloca esse dinheiro na poupança que a senhora ganha R$10 mil por mês. -Dilma: O que que é R$10 mil?</p>
+        </div>
+    </section>
 </section><!--    #destaques    -->
 @endsection {{--/conteudo da pagina--}}
