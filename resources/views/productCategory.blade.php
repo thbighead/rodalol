4e9e5a42e5014224b78fc4db544fa777701a0f12
Pablo da Rocha <pablodarocha.ribeiro@gmail.com>
2015-08-20 22:45:52 -0300

    * agora o paginate funciona pra categoria

--- resources/views/productCategory.blade.php
+++ resources/views/productCategory.blade.php
@@ -33,16 +33,16 @@
             </div>
 
             <div class="col-md-10">
-                @foreach($product as $product)
+                @foreach($product as $products)
                     <div class="col-sm-3 col-lg-3 col-md-3">
                         <div class="thumbnail">
-                            <img src="{{asset('img/products_index/product2.png')}}" alt="Imagem 1">
+                            <img src="{{asset('img/products_index/product6.png')}}" alt="Imagem 1">
                             <div class="caption text-center">
-                                <a href="{{route('detalhesProduto',$product->id)}}">
+                                <a href="{{route('detalhesProduto',$products->id)}}">
                                     {{--<p>{{$product->nome}}</p>--}}
                                     <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget </p>
                                 </a>
-                                <h3 class="text-center color-green">R${{$product->preco}}</h3>
+                                <h3 class="text-center color-green">R${{$products->preco}}</h3>
                             </div><!--  caption -->
                         </div><!--  thumbnail -->
                     </div><!--  product -->
@@ -50,12 +50,13 @@
             </div>
 
             {{--NÃO TÁ FUNCIONADO. WHY? WHY? NA PÁGINA DE LISTAGEM DE TODOS OS PRODUTOS FUNCIONA--}}
-            {{--<div class="row text-center">--}}
-                {{--{!! $product->render() !!}--}}
-            {{--</div>--}}
+
+
         </div>{{--.row--}}
 
-
+        <div class="text-center">
+            {!! $product->render() !!}
+        </div>
         {{--</div>--}}{{-- .row --}}
     </div>{{-- .container --}}
 </section>{{-- #produtos --}}
